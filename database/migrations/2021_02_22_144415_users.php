dc2b7b70e20ec762a2e2866199e872e9ae279d0d
Shapus <ostromog@mail.ru>
2021-02-22 21:11:24 +0200

    Add 'users' migration and seeder

---
+++ database/migrations/2021_02_22_144415_users.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email',255);
+            $table->string('first_name',255);
+            $table->string('last_name',255);
+            $table->string('username',255);
+            $table->timestamp('registered_at')->nullable();
+            $table->string('key', 255)->nullable();
+            $table->timestamp('key_at')->nullable();
+            $table->string('auth_type',2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            [
+                'email' => "lukas.andersson@switzerland.com",
+                'first_name' => "Lukas",
+                'last_name' => "Andersson",
+                'username' => "lukas",
+                'registered_at' => null,
+                'key' => null,
+                'key_at' => null,
+                'auth_type' => null
+            ],
+            [
+                'email' => "josy.lindberg@switzerland.com",
+                'first_name' => "Josy",
+                'last_name' => "Lindberg",
+                'username' => "josy",
+                'registered_at' => null,
+                'key' => null,
+                'key_at' => null,
+                'auth_type' => null
+            ],
+            [
+                'email' => "ernst.lindberg@switzerland.com",
+                'first_name' => "Ernst",
+                'last_name' => "Lindberg",
+                'username' => "ernst",
+                'registered_at' => null,
+                'key' => null,
+                'key_at' => null,
+                'auth_type' => null
+            ],
+            [
+                'email' => "meyer.holm@switzerland.com",
+                'first_name' => "Meyer",
+                'last_name' => "Holm",
+                'username' => "meyer",
+                'registered_at' => null,
+                'key' => null,
+                'key_at' => null,
+                'auth_type' => null
+            ]
+        ]);
+    }
+}

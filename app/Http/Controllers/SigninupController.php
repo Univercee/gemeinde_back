f702b378beee95a96054e806bdd49ec5f879f32b
ahabdu <ahabdu@ttu.ee>
2021-02-23 11:55:28 +0200

    GENA-7 form with validation and request to api and controller with validation and sql query

---
+++ app/Http/Controllers/SigninupController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SigninupController extends Controller
+{
+    public function store(Request $request){
+        $email = $request->input('email');
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return response()->json(['error' => 'Bad Email'], 400);
+        }else{
+            $queryUser = app('db')->select("SELECT * FROM users WHERE email = :email", ['email' => $email]);
+            $secretKey =  md5("salt007".$email.uniqid()."0004salt");
+            return response()->json(['key' => $queryUser]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(LocationsTableSeeder::class);
         $this->call(ServicesTableSeeder::class);
         $this->call(LocationServicesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }

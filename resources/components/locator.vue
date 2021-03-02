ebca9d4822180b4f26c693eba4a229655243ca82
Zahhar Kirillov <zahhar@gmail.com>
2021-03-02 22:25:47 +0100

    Updating landing page components

--- resources/components/locator.vue
+++ resources/components/locator.vue
@@ -11,15 +11,15 @@
 
     <div class="col-lg-3 bg-dark p-4 text-white">
         <div id="signupform">
-            <em class="fs-5">Entdecken Sie Dienstleistungen in Ihrer Gemeinde und verbinden Sie sich mit Nachbarn, regionale Unternehmen und Verwaltung in einer Minute.</em>
+            <em class="fs-6">Entdecken Sie Dienstleistungen in Ihrer Gemeinde und verbinden Sie sich mit Nachbarn, regionale Unternehmen und Verwaltung in einer Minute.</em>
             <div class="clearfix mt-5">
                 <h4 class="float-start">Mein Wohnort</h4>
                 <div class="mt-1 float-end"><a class="link-light" href="#">finde mich</a></div>
             </div>
-            <form class="d-flex needs-validation" @submit="submit" method="post" novalidate>
+            <form class="d-flex needs-validation" @submit="submit" novalidate>
                 <div class="input-group">
-                    <input class="form-control" name="location" id="location" type="search" placeholder="PLZ oder Ortsname" aria-label="Search" v-model="cred.location" required>
-                    <button class="btn btn-primary"  type="submit"><svg class="" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M19 17l-5.15-5.15a7 7 0 1 0-2 2L17 19zM3.5 8A4.5 4.5 0 1 1 8 12.5 4.5 4.5 0 0 1 3.5 8z"/></svg></button>
+                    <input class="form-control" ref="tomSelect" placeholder="PLZ oder Ortsname" v-model="searchable" required>
+                    <button class="btn btn-primary" type="submit"><svg class="" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M19 17l-5.15-5.15a7 7 0 1 0-2 2L17 19zM3.5 8A4.5 4.5 0 1 1 8 12.5 4.5 4.5 0 0 1 3.5 8z"/></svg></button>
                 </div>
                 <div class="invalid-feedback">
                     <p v-if="errors.length">
@@ -29,52 +29,6 @@
                     </p>
                 </div>
             </form>
-
-            <!-- Modal -->
-
-            <div class="modal fade" id="locationModal" tabindex="-1" aria-labelledby="modalLabel_location" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <template v-if="log =='services'">
-                                <h5 class="paragraphs" id="location_name"> {{ location_name}},</h5> &nbsp;
-                                <h5 class="paragraphs" id="region_name"> {{ location_region}}</h5>  &nbsp;
-                                <h5 class="paragraphs" id="location_zip"> ({{ cred.location}})</h5>
-                            </template>
-                            <button  type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <ul id="array-rendering">
-                                <template v-if="log == 'error'"><p class="paragraphs fw-normal fs-5">Bad input. Please write zipcode correctly! </p></template>
-
-                                <template v-if="log == 'noLoc'"><p class="paragraphs fw-normal fs-5"> There is no this location. Please fill out form! </p></template>
-                                <template v-else-if="log =='noservices'">
-                                    <p class="paragraphs fw-normal fs-5">
-                                        There is no services by {{cred.location}} zipcode. Please fill out form!
-                                    </p>
-                                </template>
-                                <template v-if="log =='services'">
-                                    <li class="paragraphs fw-normal fs-5" v-for="(item, index) in list.services" v-bind:key="index">
-                                        {{ item.name }}
-                                    </li>
-                                </template>
-
-                            </ul>
-                        </div>
-                        <div class="modal-footer">
-                            <template v-if="log =='services'">
-                                <button type="button" class="btn btn-primary">Sign up</button>
-                            </template>
-                            <template v-else-if="log =='noLoc'">
-                                <button type="button" class="btn btn-primary">Fill form</button>
-                            </template>
-                            <template v-else-if="log =='noservices'">
-                                <button type="button" class="btn btn-primary">Fill form</button>
-                            </template>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 
@@ -100,24 +54,23 @@
             locations: null,
             log: '',
             errors: [],
-            cred: {
-                location: null,
-            }
+            searchable: ''
         }
     },
 
     mounted: async function() {
-        this.initMap();
         await this.getLocations();
+        this.initMap();
+        this.initTomSelect();
         window.addEventListener("scroll", _.debounce(this.dropMarkers, 150, { 'leading': true }));
     },
 
     methods: {
+
         async getLocations() {
             await axios
                 .get('/locations')
                 .then(response => (this.locations = response.data.locations));
-            console.log(this);
         },
 
         initMap() {
@@ -130,6 +83,22 @@
             );
         },
 
+        initTomSelect() {
+            this.ts = new TomSelect(this.$refs.tomSelect, {
+                openOnFocus: false,
+                maxItems: 1,
+                valueField: 'id',
+                searchField: ['name','zipcode'],
+                labelField: 'name',
+                sortField: 'name',
+                options: this.locations
+             });
+
+            this.ts.on('change', id => {
+                this.openInfoWindow(id)
+            });
+        },
+
         dropMarkers() {
             if(!this.checkMapInViewport() || this.map.markersDropped) {
                 return;
@@ -137,17 +106,18 @@
             let m = null;
             for(let i in this.locations) {
                 let zip = this.locations[i].zipcode;
+                let id = this.locations[i].id;
                 setTimeout(() => {
                     m = new google.maps.Marker({
                         position: new google.maps.LatLng(this.locations[i].lat,this.locations[i].lng),
                         map: this.map.el,
                         zipcode: zip,
                         animation: google.maps.Animation.DROP,
-                        id: i
+                        id: id
                     });
-                    this.map.markers[i] = m;
+                    this.map.markers[id] = m;
                     m.addListener("click", () => {
-                        this.openInfoWindow(i);
+                        this.openInfoWindow(id);
                     });
                 }, i*250);
             }
@@ -184,7 +154,7 @@
                 return '<div>\
                     <h5 id="location_name">'+title+'</h5>\
                     <ul>'+services_list+'</ul>\
-                    <a href="" class="btn btn-primary btn-sm">Sign up</a>\
+                    <a href="/signup" class="btn btn-primary btn-sm">Sign up</a>\
                     </div>';
             } catch (error) {
                 console.error(error);
@@ -207,54 +177,8 @@
 	    },
 
         submit(e) {
-            this.errors = [];
-            axios.get("/locations/" + this.cred.location + "/services")
-                .then((resp) => {
-                    this.list = resp.data
-                    if('location' in resp.data) {
-                        this.location_name = resp.data.location.name;
-                        this.location_region = resp.data.location.region;
-                        this.location_zip = resp.data.location.zipcode;
-                        this.location_services = resp.data.services;
-                        this.log = 'services';
-                        var key = this.location_zip;
-                        map_intsance.infowindow.open(map_intsance.map, map_intsance.markers[key]);
-                        var location_string = this.location_name+', '+this.location_region+' ('+this.location_zip+')';
-                        var services_string = "";
-                        for(var i=0; i<this.location_services.length; i++) {
-                            services_string += '<li class="fw-normal fs-5">'+this.location_services[i].name+'</li>';
-                        }
-                        var infowindow_content = '<div>\
-                        <div class="modal-header"> \
-                            <h3 id="location_name">'+location_string+'</h3>\
-                            </div>\
-                            <div class="modal-body"> \
-                            <ul id="array-rendering">'+services_string+'</ul>\
-                            </div>\
-                            <div class="modal-footer"> \
-                            <div class="d-flex justify-content-end">\
-                              <a href="" class="btn btn-primary">Sign up</a>\
-                            </div>\
-                          </div>';
-                        map_intsance.infowindow.setContent(infowindow_content);
-                    } else {
-                        this.log = 'noservices';
-                    }
-                    document.getElementById("location").classList.remove("is-invalid");
-
-                })
-                .catch((err) => {
-                    if(!this.cred.location || err.response.status === 400) {
-                        this.errors.push('Write valid zipcode pls');
-                        this.log = 'error';
-                        document.getElementById("location").classList.add("is-invalid");
-                    } else if (err.response.status === 404) {
-                        this.log = 'noLoc';
-                        return;
-                    }
-                    this.list = [];
-                });
             e.preventDefault();
+            console.log(e);
         }
     }
 }
@@ -264,5 +188,9 @@
   #mapWrapper {
     height: 400px;
     width: 100%;
+  }
+
+  .ts-control.single .ts-input:after {
+      content: ''
   }
 </style>

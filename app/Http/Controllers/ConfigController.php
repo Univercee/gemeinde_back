0efad3f869875a054672f5ec7b7d0e89b2548888
ahabdu <ahabdu@ttu.ee>
2021-03-04 13:06:09 +0200

    GENA-27 recapchav3 env controller

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -4,11 +4,10 @@
 namespace App\Http\Controllers;
 
 
-use Illuminate\Http\Request;
 
 class ConfigController extends Controller
 {
-    public function getKeys(Request $request){
+    public function getKeys(){
         $keys = ["googleRecaptchaSiteKey" => "6LehX2caAAAAAKKV2_vQTCGkh4-y4LHTjkTEfAQC",
                 "googleMapKey" => "lool", "tgBotName" => "1592850338:AAH1NeLiWUW9jy6861AZmjuG6JRdqtywyhw"];
         return response()->json($keys);

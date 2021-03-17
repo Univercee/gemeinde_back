a99570395cbc21581777f3aa4576aff3e648eab4
Shapus <ostromog@mail.ru>
2021-03-17 12:22:02 +0200

    Add APP_ROOT key to ConfigController

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -10,7 +10,8 @@ class ConfigController extends Controller
     public function getKeys(){
         $keys = ["googleRecaptchaSiteKey" => getenv('GOOGLE_RECAPTCHA_SITE_KEY'),
                 "googleMapKey" => getenv('GOOGLE_MAP_KEY'),
-                "tgBotName" => getenv('TG_BOT_NAME')];
+                "tgBotName" => getenv('TG_BOT_NAME'),
+                "appRoot" => getenv('APP_ROOT')];
         return response()->json($keys);
     }
 }

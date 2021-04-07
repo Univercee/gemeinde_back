0ad6d6388965dcbc89dfd70a484e6157ea2e3ec5
ahabdu <ahabdu@ttu.ee>
2021-04-07 17:07:45 +0300

    GENA-46 refractor a bit

---
+++ resources/components/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="test">
+    <h1>TEST</h1>
+    {{ $t("message.hello") }}
+  </div>
+</template>
+
+<script>
+export default {
+name: "test",
+  data() {
+    return {
+      lang: null,
+      test: null,
+    }
+  }, methods: {
+    langDetect(){
+      let href = window.location.href.split("/").pop()
+      if(href == 'en' || href == 'de') {
+        this.lang = href
+      }else {
+        this.lang = navigator.language.split('-')[0]
+      }
+      return this.lang
+    }
+  }, mounted(){
+    this.test = this.langDetect()
+    alert(this.test)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/lang/en/auth.php
@@ -0,0 +1,18 @@
+<?php
+return [
+  'welcome'=>"Welcome bro",
+  'about'=>'about',
+  'verifyLinkMsg'=>'We sent you a verification email with a link, please find in you inbox',
+  'keyNotFound'=>'Verification key is not found or already used by you, please try again',
+  'keyExpired'=>'Key has expired',
+  'gzRegMsg'=>'Congratulations! You are successfully registered now and may proceed to your profile.',
+  'userLoggedIn'=>'User has been logged in',
+  'botError'=>'You are robot, dont event try! I am a teapot.',
+  'w5min'=>'Please wait at least 5 min',
+  'mailSent'=>'Registration email sent',
+  'mailVerified'=>'Registration email sent',
+  'notFound'=>'Not found',
+  'channelAdded'=>'Channel has been added',
+  'notTgData'=>'Data is NOT from Telegram',
+  'outDateData'=>'Data is NOT from Telegram',
+];

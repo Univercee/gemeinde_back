4fcfce6a12519887c69463c887bfc1abb81767ac
ahabdu <ahabdu@ttu.ee>
2021-06-28 16:53:42 +0300

    delete assests folder

--- resources/assets/js/axios-defaults.js
+++
@@ -1,4 +0,0 @@
-axios.defaults.baseURL = '/api';
-axios.defaults.headers.common['Content-Type'] = 'application/json'
-let sessionKey = sessionStorage.getItem('sessionKey');
-if(sessionKey) axios.defaults.headers.common.Authorization = 'Bearer '+sessionKey

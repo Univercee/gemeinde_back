e831aed0ce3097f75f9ba6719aab05e733a0ff57
Aleksandr Ostromogilskii <60792658+Shapus@users.noreply.github.com>
2021-09-06 22:50:16 +0300

    replace source with service_id

--- app/Managers/EventManager.php
+++ app/Managers/EventManager.php
@@ -4,9 +4,9 @@ namespace App\Managers;
 use Illuminate\Support\Facades\DB;
 class EventManager
 {
-    public static function addEvent($location_id, $source, $valid_from, $valid_until, $title_en, $text_en, $title_de, $text_de, $external_id = null){
+    public static function addEvent($location_id, $service_id, $valid_from, $valid_until, $title_en, $text_en, $title_de, $text_de, $external_id = null){
         DB::table('events')->insert(['location_id'=>$location_id,
-                                    'source'=>$source, 
+                                    'service_id'=>$service_id, 
                                     'valid_from'=>$valid_from, 
                                     'valid_until'=>$valid_until,
                                     'title_en'=>$title_en,
